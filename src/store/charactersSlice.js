4387bf7357fab70973298ab6ef793fa4b6230bb7
Tony Farrelly <tfarrelly01@gmail.com>
2022-08-04 20:26:13 +0100

    Async Thunks with Redux Toolkit

--- src/store/charactersSlice.js
+++ src/store/charactersSlice.js
@@ -1,9 +1,19 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   data: [],
-  loading: true,
+  loading: false,
 };
+
+const ENDPOINT = "https://star-wars-character-search.glitch.me/api/search/";
+
+export const fetchCharactersFromAPI = createAsyncThunk(
+  'characters/fetchCharacters',
+  async (searchTerm) => {
+    const response = await fetch(ENDPOINT + searchTerm).then(response => response.json());
+    return response.characters;
+  },
+);
 
 export const charactersSlice = createSlice({
   name: 'characters',
@@ -11,6 +21,15 @@ export const charactersSlice = createSlice({
   reducers: {
     add: (state, action) => {
       state.characters = action.payload;
+    },
+  },
+  extraReducers: {
+    [fetchCharactersFromAPI.pending]: (state, action) => {
+      state.loading = true;
+    },
+    [fetchCharactersFromAPI.fulfilled]: (state, action) => {
+      state.data = action.payload;
+      state.loading = false;
     },
   },
 });
